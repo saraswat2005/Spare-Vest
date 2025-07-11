13db676535d731453d69b3473e42cf458de87766
Shubham Saraswat <141159989+saraswat2005@users.noreply.github.com>
2025-07-11 12:37:23 +0530

    Delete 1_layout.py

--- 1_layout.py
+++
@@ -1,9 +0,0 @@
-from appJar import gui  
-def changeLabel(btn):  
-      app.setLabel("l2", app.getEntry("text"))  
-app = gui()  
-app.addLabel("l1", "Simple Demo")
-app.addEntry("text")
-app.addButton("OK", changeLabel)
-app.addEmptyLabel("l2")
-app.go()
